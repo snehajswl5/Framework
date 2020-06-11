d7b361715eb984c3a2e5c416b2af60d230471f61
snehajswl5 <snehajswl5@gmail.com>
2020-06-11 12:58:03 +0530

    a new code committed

--- src/test/java/com/forum/utility/ConfigDataProvider.java
+++ src/test/java/com/forum/utility/ConfigDataProvider.java
@@ -2,8 +2,6 @@ package com.forum.utility;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.util.Properties;
 
 public class ConfigDataProvider {
